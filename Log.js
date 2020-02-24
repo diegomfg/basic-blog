0a9e5587dbcdf45ea18961600d518c6f26d8b85d
diego <diegoamatheus@gmail.com>
2020-02-24 00:58:05 -0500

    Second Commit

---
+++ Log.js
@@ -0,0 +1,20 @@
+
+    /**
+     *  @Date modified 02/23/2020
+     * 
+     *  Updated By: Diego Matheus
+     * 
+     *  @summary
+     * 
+     *  Packages were updated, and by doing so, 
+     *  a couple of the template engine's outdated instructions broke, 
+     *  causing a forced debug and update of these mentioned instructions.
+     * 
+     *  *TIP*
+     * 
+     *  Refactor into async - await.
+     *  Build the client side on React or Angular
+     *  Refactor the API and the controllers-routes.
+     */
+
+
--- app.js
+++ app.js
@@ -119,23 +119,6 @@ app.put('/blogs/:id', async (req, res)=>{
 
 app.delete('/blogs/:id', async (req, res)=>{
 
-    /**
-     *  @Date modified 02/23/2020
-     * 
-     *  Updated By: Diego Matheus
-     * 
-     *  @summary
-     * 
-     *  Packages were updated, and by doing so, 
-     *  a couple of the template engine's outdated instructions broke, 
-     *  causing a forced debug and update of these mentioned instructions.
-     * 
-     *  *TIP*
-     * 
-     *  Refactor into async - await.
-     *  Build the client side on React or Angular
-     *  Refactor the API and the controllers-routes.
-     */
    try {
         await blog.findByIdAndRemove(req.params.id);
         return redirect("/blogs")
